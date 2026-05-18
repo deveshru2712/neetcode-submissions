dc2afc3eef8d3d8b2bfc06b09fdaf904b0494bd3
Devesh Chandra <deveshru2712@gmail.com>
2026-05-18 10:41:59 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==val){
+                nums[i]+=51;
+            }
+        }
+        sort(nums.begin(),nums.end());
+        int cnt = 0;
+        for(auto it:nums){
+            if(it>=51) break;
+            else cnt++;
+        }
+        return cnt;
+    }
+};
